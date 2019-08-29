3db93b65f0454f9f5c705acb3168431afedb5edd
jfcarocota <jfcarocota@gmail.com>
2019-08-29 08:27:05 -0700

    grab dectection

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    [SerializeField]
+    Transform grabPoint;
+
+    [SerializeField]
+    Transform middleTip;
+    [SerializeField]
+    Transform thumbTip;
+    [SerializeField]
+    float minDistance;
+
+    /* void Update()
+    {
+        if(IsGrabing)
+        {
+            Debug.Log("Grab");
+        }
+    }*/
+
+    bool IsGrabing
+    {
+        get => Vector3.Distance(thumbTip.position, middleTip.position) <= minDistance;
+    }
+}
